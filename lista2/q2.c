50fbb19de26f40bf2d445d62ced35c4f8cb6c9c0
Saulo <saulomiqueias7@gmail.com>
2021-09-28 00:46:25 -0300

    done

--- lista2/q2.c
+++ lista2/q2.c
@@ -17,10 +17,14 @@ int main(int argc, char const *argv[])
         printf("Homem ");
         printf("Seu peso ideal: %f\n", (72.7 * altura) - 58);
     }
-    else
+    else if (sexo == 'M')
     {
         printf("Mulher");
         printf("Seu peso ideal: %f\n", (62.1 * altura) - 44.7);
+    }
+    else
+    {
+        printf("Informe uma opção de sexo valida.");
     }
     return 0;
 }
